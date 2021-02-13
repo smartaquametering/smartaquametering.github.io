ab076e1830792524ef9a68852ae87c17551fa7d0
Joerg Heinemann <heinemannj66@gmail.com>
2021-02-13 06:01:59 +0100

    Initial commit

--- assets/js/ColorScales.js
+++ assets/js/ColorScales.js
@@ -1,129 +1,224 @@
 // Dummy Color Index
+//
 var None = {};
 
 // CIE-L*a*b* coordinates of Iodine Color Number
-// D65/2°/2°
-var IodineColorIndex = {
-	'IodineColor-1': '100, -3, 6',
-	'IodineColor-3': '100, -7, 18',
-	'IodineColor-5': '100, -9, 27',
-	'IodineColor-7': '100, -11, 37',
-	'IodineColor-10': '100, -11, 59',
-	'IodineColor-20': '100, -10, 72',
-	'IodineColor-90': '100, 20, 103'
+//
+var IodineColor = {
+	Name: 'Iodine Color Number',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+		'IodineColor-1': '100, -3, 6',
+		'IodineColor-3': '100, -7, 18',
+		'IodineColor-5': '100, -9, 27',
+		'IodineColor-7': '100, -11, 37',
+		'IodineColor-10': '100, -11, 59',
+		'IodineColor-20': '100, -10, 72',
+		'IodineColor-90': '100, 20, 103'
+	}
 };
 
 // CIE-L*a*b* coordinates of Hazen/APHA/PtCo Color Number
-// D65/2°
-var PtCoColorIndex = {
+//
+var PtCoColor = {
+	Name: 'Hazen/APHA/PtCo Color Number',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
 };
 
 // CIE-L*a*b* coordinates of Gardner Color Number
-// D65/2°
-var GardnerColorIndex = {
-}
+//
+var GardnerColor = {
+	Name: 'Gardner Color Number',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of Lovibond Color System
-// D65/2°
-var LovibondColorIndex = {
-}
+//
+var LovibondColor = {
+	Name: 'Lovibond Color System',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of European Pharmacopoeia (EP) Color Determination
-// D65/2°
-var EuropeanPharmacopoeiaColorIndex = {
-}
+//
+var EuropeanPharmacopoeiaColor = {
+	Name: 'European Pharmacopoeia (EP) Color Determination',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of US Pharmacopoeia (USP) Color Determination
-// D65/2°
-var USPharmacopoeiaColorIndex = {
-}
+//
+var USPharmacopoeiaColor = {
+	Name: 'US Pharmacopoeia (USP) Color Determination',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of Chinese Pharmacopoeia (CP) Color Determination
-// D65/2°
-var ChinesePharmacopoeiaColorIndex = {
-}
+//
+var ChinesePharmacopoeiaColor = {
+	Name: 'Chinese Pharmacopoeia (CP) Color Determination',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of Klett Color Number
-// D65/2°
-var KlettColorIndex = {
-}
+//
+var KlettColor = {
+	Name: 'Klett Color Number',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of Hess-Ives Color Number
-// D65/2°
-var HessIvesColorIndex = {
-}
+//
+var HessIvesColor = {
+	Name: 'Hess-Ives Color Number',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of Yellowness Index (ASTM D1925)
-// D65/2°
-var YellownessColorIndex = {
-}
+//
+var YellownessColor = {
+	Name: 'Yellowness Index (ASTM D1925)',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of ADMI Color Number
-// D65/2°
-var ADMIColorIndex = {
-}
+//
+var ADMIColor = {
+	Name: 'ADMI Color Number',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of Acid Wash Color Determination
-// D65/2°
-var AcidWashColorIndex = {
-}
+//
+var AcidWashColor = {
+	Name: 'Acid Wash Color Determination',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of EBC Brewery Color Number
-// D65/2°
-var EBCBreweryColorIndex = {
-}
+//
+var EBCBreweryColor = {
+	Name: 'EBC Brewery Color Number',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of ASBC Brewery Color Number
-// D65/2°
-var ASBCBreweryColorIndex = {
-}
+//
+var ASBCBreweryColor = {
+	Name: 'ASBC Brewery Color Number',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of ASTM-Farbzahl
-// D65/2°
-var ASTMColorIndex = {
-}
+//
+var ASTMColor = {
+	Name: 'ASTM-Farbzahl',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of Saybolt-Farbzahl
-// D65/2°
-var SayboltColorIndex = {
-}
+//
+var SayboltColor = {
+	Name: 'Saybolt-Farbzahl',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of ICUMSA Zuckerfarbe
-// D65/2°
-var ICUMSASuguarColorIndex = {
-}
+//
+var ICUMSASuguarColor = {
+	Name: 'ICUMSA Zuckerfarbe',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of SAC436
-// D65/2°
-var SAC436ColorIndex = {
-}
+//
+var SAC436Color = {
+	Name: 'SAC436',
+	ColorSpace: 'CIE-L*a*b*',
+	Reference: 'D65/2°',
+	Index: {
+	}
+};
 
 // CIE-L*a*b* coordinates of ColorChecker Patches (2005)
-// D65/2°
-var ColorCheckerIndex = {
+//
+var ColorChecker = {
+	Name: 'ColorChecker Patches (2005)',
+	ColorSpace: 'CIE-L*a*b*',
 	Reference: 'D65/2°',
-	'ColorChecker-1-Dark-Skin': '37.77320582, 12.59128198, 13.73769561',
-	'ColorChecker-2-Light-Skin': '65.43191905, 16.84716181, 17.38241346',
-	'ColorChecker-3-Blue-Sky': '50.22174301, -2.30111402, -21.54911359',
-	'ColorChecker-4-Foliage': '43.04120381, -14.9435302, 22.05079801',
-	'ColorChecker-5-Blue-Flower': '55.36543671, 11.46277348, -25.15763139',
-	'ColorChecker-6-Bluish-Green': '70.91359483, -33.08127098, 0.267569665',
-	'ColorChecker-7-Orange': '62.03635424, 33.60758734, 56.36103711',
-	'ColorChecker-8-Purplish-Blue': '40.6608605, 16.04386551, -45.13955717',
-	'ColorChecker-9-Moderate-Red': '50.61971705, 47.69824032, 15.19530812',
-	'ColorChecker-10-Purple': '30.44292876, 24.88978119, -21.58988957',
-	'ColorChecker-11-Yellow-Green': '72.28547874, -27.94505961, 57.86987406',
-	'ColorChecker-12-Orange-Yellow': '71.3861192, 15.89028483, 67.70785468',
-	'ColorChecker-13-Blue': '29.57402748, 20.59672549, -49.21016282',
-	'ColorChecker-14-Green': '55.23286542, -41.2087517, 31.93417479',
-	'ColorChecker-15-Red': '41.46806294, 52.74966561, 26.8685097',
-	'ColorChecker-16-Yellow': '81.22210753, -0.439269695, 80.26582009',
-	'ColorChecker-17-Magenta': '51.72649032, 51.21386006, -15.23923141',
-	'ColorChecker-18-Cyan': '51.56289275, -23.96845056, -27.84527594',
-	'ColorChecker-19-White': '96.5298366, -0.534422137, 1.181524596',
-	'ColorChecker-20-Neutral-8': '81.26440787, -0.604227742, -0.322113965',
-	'ColorChecker-21-Neutral-6.5': '66.77572154, -0.683791931, -0.488050042',
-	'ColorChecker-22-Neutral-5': '50.87069005, -0.127084092, -0.265109789',
-	'ColorChecker-23-Neutral-3.5': '35.67120505, -0.302024432, -1.212622414',
-	'ColorChecker-24-Black': '20.47143293, 0.013936713, -0.962268947'
+	Index: {
+		'ColorChecker-1-Dark-Skin': '37.77320582, 12.59128198, 13.73769561',
+		'ColorChecker-2-Light-Skin': '65.43191905, 16.84716181, 17.38241346',
+		'ColorChecker-3-Blue-Sky': '50.22174301, -2.30111402, -21.54911359',
+		'ColorChecker-4-Foliage': '43.04120381, -14.9435302, 22.05079801',
+		'ColorChecker-5-Blue-Flower': '55.36543671, 11.46277348, -25.15763139',
+		'ColorChecker-6-Bluish-Green': '70.91359483, -33.08127098, 0.267569665',
+		'ColorChecker-7-Orange': '62.03635424, 33.60758734, 56.36103711',
+		'ColorChecker-8-Purplish-Blue': '40.6608605, 16.04386551, -45.13955717',
+		'ColorChecker-9-Moderate-Red': '50.61971705, 47.69824032, 15.19530812',
+		'ColorChecker-10-Purple': '30.44292876, 24.88978119, -21.58988957',
+		'ColorChecker-11-Yellow-Green': '72.28547874, -27.94505961, 57.86987406',
+		'ColorChecker-12-Orange-Yellow': '71.3861192, 15.89028483, 67.70785468',
+		'ColorChecker-13-Blue': '29.57402748, 20.59672549, -49.21016282',
+		'ColorChecker-14-Green': '55.23286542, -41.2087517, 31.93417479',
+		'ColorChecker-15-Red': '41.46806294, 52.74966561, 26.8685097',
+		'ColorChecker-16-Yellow': '81.22210753, -0.439269695, 80.26582009',
+		'ColorChecker-17-Magenta': '51.72649032, 51.21386006, -15.23923141',
+		'ColorChecker-18-Cyan': '51.56289275, -23.96845056, -27.84527594',
+		'ColorChecker-19-White': '96.5298366, -0.534422137, 1.181524596',
+		'ColorChecker-20-Neutral-8': '81.26440787, -0.604227742, -0.322113965',
+		'ColorChecker-21-Neutral-6.5': '66.77572154, -0.683791931, -0.488050042',
+		'ColorChecker-22-Neutral-5': '50.87069005, -0.127084092, -0.265109789',
+		'ColorChecker-23-Neutral-3.5': '35.67120505, -0.302024432, -1.212622414',
+		'ColorChecker-24-Black': '20.47143293, 0.013936713, -0.962268947'
+	}
 };
