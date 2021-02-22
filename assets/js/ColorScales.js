5018698884f2abe901e0cdc8c75b567d0628e102
Joerg Heinemann <heinemannj66@gmail.com>
2021-02-22 09:47:50 +0100

    Initial commit

--- assets/js/ColorScales.js
+++ assets/js/ColorScales.js
@@ -238,7 +238,7 @@ function GetColorScale(SelectedColorScale)
 
 			var Cx = 1.2769;
 			var Cz = 1.0592;
-			var YI = (100 * (Cx * X - Cz * Z)) / Y;
+			var YI = (100 * (Cx * SensorXYZ.X - Cz * SensorXYZ.Z)) / SensorXYZ.Y;
 
 			ColorScale = {
 				Name: 'Yellowness Index',
